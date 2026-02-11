0be1ab3f15ba158969736fc6c0c7bfff53ce2082
Hypirius <zuhayerhossain25@gmail.com>
2026-02-11 14:11:34 +0600

    feat: add Home text and links

--- src/route-components/Home/Home.tsx
+++ src/route-components/Home/Home.tsx
@@ -1,5 +1,17 @@
+import "./Home.css"
+import Logo from "../../assets/React.svg?react"
+import { Link } from "react-router"
+
 function Home() {
-  return <div>Home</div>
+  return (
+    <div id="home-container">
+      <Logo width={100} height={100} />
+      <p>The library for web and native user interfaces</p>
+      <button>
+        <Link to="/learn">Learn React</Link>
+      </button>
+    </div>
+  )
 }
 
 export default Home
